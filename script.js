12b89cf6e89193fc0a3051c3f1507b4b2ca14077
Muhammad Dody Pratama <dodypratama252@gmail.com>
2024-06-27 18:07:25 +0700

    Build First Version
    First Version Of Hiragana and Katakana Test Flash Card

---
+++ script.js
@@ -0,0 +1,135 @@
+const hiraganaKatakanaMap = {
+  あ: "a",
+  い: "i",
+  う: "u",
+  え: "e",
+  お: "o",
+  か: "ka",
+  き: "ki",
+  く: "ku",
+  け: "ke",
+  こ: "ko",
+  さ: "sa",
+  し: "shi",
+  す: "su",
+  せ: "se",
+  そ: "so",
+  た: "ta",
+  ち: "chi",
+  つ: "tsu",
+  て: "te",
+  と: "to",
+  な: "na",
+  に: "ni",
+  ぬ: "nu",
+  ね: "ne",
+  の: "no",
+  は: "ha",
+  ひ: "hi",
+  ふ: "fu",
+  へ: "he",
+  ほ: "ho",
+  ま: "ma",
+  み: "mi",
+  む: "mu",
+  め: "me",
+  も: "mo",
+  や: "ya",
+  ゆ: "yu",
+  よ: "yo",
+  ら: "ra",
+  り: "ri",
+  る: "ru",
+  れ: "re",
+  ろ: "ro",
+  わ: "wa",
+  を: "wo",
+  ん: "n",
+  ア: "a",
+  イ: "i",
+  ウ: "u",
+  エ: "e",
+  オ: "o",
+  カ: "ka",
+  キ: "ki",
+  ク: "ku",
+  ケ: "ke",
+  コ: "ko",
+  サ: "sa",
+  シ: "shi",
+  ス: "su",
+  セ: "se",
+  ソ: "so",
+  タ: "ta",
+  チ: "chi",
+  ツ: "tsu",
+  テ: "te",
+  ト: "to",
+  ナ: "na",
+  ニ: "ni",
+  ヌ: "nu",
+  ネ: "ne",
+  ノ: "no",
+  ハ: "ha",
+  ヒ: "hi",
+  フ: "fu",
+  ヘ: "he",
+  ホ: "ho",
+  マ: "ma",
+  ミ: "mi",
+  ム: "mu",
+  メ: "me",
+  モ: "mo",
+  ヤ: "ya",
+  ユ: "yu",
+  ヨ: "yo",
+  ラ: "ra",
+  リ: "ri",
+  ル: "ru",
+  レ: "re",
+  ロ: "ro",
+  ワ: "wa",
+  ヲ: "wo",
+  ン: "n",
+};
+
+let currentQuestion = "";
+
+function getRandomCharacter() {
+  const keys = Object.keys(hiraganaKatakanaMap);
+  return keys[Math.floor(Math.random() * keys.length)];
+}
+
+function showQuestion() {
+  currentQuestion = getRandomCharacter();
+  $("#question").text(currentQuestion);
+  $("#answer").val("").focus();
+}
+
+function checkAnswer() {
+  const answer = $("#answer").val().trim().toLowerCase();
+  const correctAnswer = hiraganaKatakanaMap[currentQuestion];
+  if (answer === correctAnswer) {
+    $("#result").text("Correct!").removeClass("wrong").addClass("correct");
+  } else {
+    $("#result")
+      .text(`Wrong! Correct answer: ${correctAnswer}`)
+      .removeClass("correct")
+      .addClass("wrong");
+  }
+  setTimeout(() => {
+    $("#result").text("").removeClass("correct wrong");
+    showQuestion();
+  }, 2000);
+}
+
+$(document).ready(() => {
+  showQuestion();
+  $("#submit").click(checkAnswer);
+  $("#answer").keypress((e) => {
+    if (e.which === 13) {
+      checkAnswer();
+      e.preventDefault();
+    }
+  });
+});
